d62a364b31cc4e1633ea52df8f6d4b94d4cc7955
yoszimitsu <cotam07@gmail.com>
2021-01-18 22:28:07 +0100

    ProductController refactor

--- src/main/java/com/bar/product/controller/ProductController.java
+++ src/main/java/com/bar/product/controller/ProductController.java
@@ -6,44 +6,50 @@ import com.bar.product.request.ProductRequest;
 import com.bar.product.service.ProductService;
 import com.bar.system.endpoint.Endpoint;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController()
 @RequestMapping(ProductController.PRODUCT_ENDPOINT)
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class ProductController {
 
-    protected static final String PRODUCT_ENDPOINT = Endpoint.API_ROOT + Endpoint.URN_PRODUCT;
+    static final String PRODUCT_ENDPOINT = Endpoint.API_ROOT + Endpoint.URN_PRODUCT;
     private final ProductService productService;
     private final ProductMapper productMapper;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ProductDto get(@PathVariable long id) {
         log.info("GET " + PRODUCT_ENDPOINT + "/" + id);
         return productMapper.mapToDto(productService.get(id));
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     List<ProductDto> getAll() {
         log.info("GET " + PRODUCT_ENDPOINT);
         return productMapper.mapToDtoList(productService.getALl());
     }
 
-    @PostMapping()
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     ResponseEntity<ProductDto> post(@Valid @RequestBody ProductRequest productRequest) {
         log.info("POST " + PRODUCT_ENDPOINT);
         return ResponseEntity
@@ -51,14 +57,20 @@ public class ProductController {
                 .body(productMapper.mapToDto(productService.add(productRequest)));
     }
 
-    @PutMapping(value = "/{id}")
-    ProductDto put(@PathVariable long id,
-                   @Valid @RequestBody ProductRequest productRequest) {
+    @PutMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ProductDto put(@PathVariable long id, @Valid @RequestBody ProductRequest productRequest) {
         log.info("PUT " + PRODUCT_ENDPOINT + "/" + id);
         return productMapper.mapToDto(productService.update(id, productRequest));
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     void delete(@PathVariable long id) {
         log.info("DELETE " + PRODUCT_ENDPOINT + "/" + id);
         productService.delete(id);
