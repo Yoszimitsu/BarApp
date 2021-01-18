f46e8a1d15cc120e2bff21381def91bd2317d27f
yoszimitsu <cotam07@gmail.com>
2021-01-18 22:32:40 +0100

    added custom OrderMapper, OrderRepository and OrderService

---
+++ src/main/java/com/bar/order/mapper/OrderMapper.java
@@ -0,0 +1,64 @@
+package com.bar.order.mapper;
+
+import com.bar.order.dto.OrderDto;
+import com.bar.order.entity.Order;
+import com.bar.orderitems.mapper.OrderItemMapper;
+import com.bar.orderitems.service.OrderItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderMapper {
+
+    private final OrderItemService orderItemService;
+    private final OrderItemMapper orderItemMapper;
+
+    public OrderDto mapToDto(Order order) {
+        var orderDto = OrderDto.builder()
+                .id(order.getId())
+                .employeeId(order.getEmployeeId())
+                .dateTime(order.getDateTime())
+                .orderItemList(orderItemMapper.mapToDtoList(orderItemService.getList(order.getId())))
+                .build();
+        return orderDto;
+    }
+
+    /**
+     * Default mapStruct.Mapper library implementation.
+     * Method maps OrderDto to Order object.
+     *
+     */
+    public Order mapToEntity(OrderDto orderDto) {
+        if (orderDto == null) {
+            return null;
+        }
+
+        Order order = new Order();
+
+        order.setId(orderDto.getId());
+        order.setEmployeeId(orderDto.getEmployeeId());
+        order.setDateTime(orderDto.getDateTime());
+        return order;
+    }
+
+    /**
+     * Default mapStruct.Mapper library implementation.
+     * Method maps List of Order to List of OrderDto collection.
+     *
+     */
+    public List<OrderDto> mapToOrderDtoList(List<Order> orderList) {
+        if (orderList == null) {
+            return null;
+        }
+
+        List<OrderDto> list = new ArrayList<OrderDto>(orderList.size());
+        for (Order order : orderList) {
+            list.add(mapToDto(order));
+        }
+        return list;
+    }
+}
