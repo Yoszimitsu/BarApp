9bfd4c22d3a90adba5bdf7d7eb2f12c9cfe640b4
yoszimitsu <cotam07@gmail.com>
2021-01-18 22:35:43 +0100

    refactor

--- src/main/java/com/bar/BarAppApplication.java
+++ src/main/java/com/bar/BarAppApplication.java
@@ -2,14 +2,13 @@ package com.bar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan({"com.bar.product.request"})
+@RestController
 public class BarAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BarAppApplication.class, args);
     }
-
 }
