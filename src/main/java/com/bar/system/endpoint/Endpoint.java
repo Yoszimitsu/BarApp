4f187cc7f03f8baef7367e62d970746a7767e537
yoszimitsu <cotam07@gmail.com>
2021-01-18 22:33:07 +0100

    added Order REST controller

--- src/main/java/com/bar/system/endpoint/Endpoint.java
+++ src/main/java/com/bar/system/endpoint/Endpoint.java
@@ -4,4 +4,5 @@ public class Endpoint {
 
     public static final String API_ROOT = "/api";
     public static final String URN_PRODUCT = "/product";
+    public static final String URN_ORDER = "/order";
 }
