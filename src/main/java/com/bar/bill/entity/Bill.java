c4b51c9017dfbde7887a6e368d42414eb501f0e0
yoszimitsu <cotam07@gmail.com>
2021-01-19 12:16:11 +0100

    added bill entity, dto, request

---
+++ src/main/java/com/bar/bill/entity/Bill.java
@@ -0,0 +1,59 @@
+package com.bar.bill.entity;
+
+import com.bar.order.entity.Order;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bills")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bill_id", nullable = false)
+    @NotNull
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    @NotBlank
+    private Order order;
+
+    @Column(name = "company", nullable = false)
+    @NotBlank
+    private String company;
+
+    @Column(name = "bill_time", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @Column(name = "price_net", nullable = false)
+    @NotNull
+    private double priceNet;
+
+    @Column(name = "nip")
+    private String nipNumber;
+
+    @Column(name = "customer_name")
+    private String customerName;
+}
---
+++ src/main/java/com/bar/bill/request/BillRequest.java
@@ -0,0 +1,29 @@
+package com.bar.bill.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class BillRequest {
+
+    @NotNull(message = "Order ID must not be empty.")
+    private long orderId;
+    @NotNull(message = "Company name must not be empty.")
+    private String company;
+    @NotNull(message = "Data and Time must not be empty.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime dateTime;
+    @NotNull(message = "Price net must not be empty.")
+    private double priceNet;
+    private String nipNumber;
+    private String customerName;
+}
