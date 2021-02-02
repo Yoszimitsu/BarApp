1d737446602bd81e147808987db3b0d884b45fb5
yoszimitsu <cotam07@gmail.com>
2021-02-02 23:55:54 +0100

    added BillControllerTest (integration-test)

--- src/main/java/com/bar/bill/request/BillRequest.java
+++ src/main/java/com/bar/bill/request/BillRequest.java
@@ -4,19 +4,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class BillRequest {
 
     @NotNull(message = "Order ID must not be empty.")
-    private long orderId;
+    private Long orderId;
     @NotNull(message = "Company name must not be empty.")
     private String company;
     @NotNull(message = "Data and Time must not be empty.")
